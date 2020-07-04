7a271ad657a334039682ae6dc216364e7d9a21d5
Justin Yoon <jihuanyoon@gmail.com>
2020-07-04 16:41:28 -0500

    Mostly working, almost finished.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # etchasketch
+ETCH-A-SKETCH Project for Odin Project curriculum
+
+To-Do:
+1. Fix numbers over 20 completely breaking the whole thing.
--- script.js
+++ script.js
@@ -5,13 +5,43 @@ function generateGrid() {
     for(let i = 0; i < sideLength; i++)
     {
         const rowDiv = document.createElement('div'); 
-        rowDiv.id = "row" + (i + 1);
         gridContainer.appendChild(rowDiv) 
         for(let j = 0; j < sideLength; j++)
         {
             const square = document.createElement('button');
-            square.className = "etchSquare";
+            square.classList.add('square');
+            square.style.height = 600 / sideLength + "px";
+            square.style.width = 600 / sideLength + "px";
+            square.addEventListener('mouseover', () => {
+                square.classList.add('filled');
+            });
             rowDiv.appendChild(square);
         }
     }
 }
+
+generateGrid();
+
+const btnReset = document.querySelector('#reset');
+btnReset.addEventListener('click', () => {
+    console.log("grid cleared.");
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((s) =>{
+        s.classList.remove('filled');
+    });
+});
+
+const btnChange = document.querySelector('#change-size');
+btnChange.addEventListener('click', () => {
+    let size = "bruh";
+    while(isNaN(size))
+    {
+        size = prompt("Enter side length. Anything over 20 starts breaking i don't know why I'll fix it later Kapp");
+    }
+    sideLength = size;
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((s) =>{
+        s.remove();
+    });
+    generateGrid()
+});
